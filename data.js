a1515aec44eceb5675ef8fab7ea52f51e4509fb0
Lebowskigrande <jclark.gfadp@gmail.com>
2025-09-29 19:39:14 -0700

    feat: update data.js from dashboard

--- data.js
+++ data.js
@@ -3670,9 +3670,9 @@ window.DATA = {
           "title": "Of Books and Men - An Overdue Adventure",
           "code": "FR-DC-RWIE-02",
           "dm": "Simon R.",
-          "gp_plus": 1000,
+          "gp_plus": 10,
           "gp_minus": 0,
-          "gp_net": 1000,
+          "gp_net": 10,
           "dtd_plus": 10,
           "dtd_minus": 0,
           "dtd_net": 10,
@@ -21299,7 +21299,7 @@ window.DATA = {
     },
     "Ecthelion": {
       "sessions": 5,
-      "net_gp": 4950,
+      "net_gp": 3960,
       "net_dtd": 33,
       "level_ups": 6,
       "perm_count": 26,
@@ -21681,7 +21681,7 @@ window.DATA = {
   },
   "meta": {
     "source_file": "D&D AL Logsheets (1).xlsx",
-    "generated": "2025-09-30T02:16:03.241Z",
+    "generated": "2025-09-30T02:39:11.546Z",
     "problems": []
   }
 };
